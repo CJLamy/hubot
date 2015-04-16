3c709b263ec89f48408da71cbffbc3ad25217ed2
Cedric Lamy <lamycedric@gmail.com>
2015-04-16 15:02:04 -0700

    removed my scripts

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,17 @@
-//  https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=QUERY_HERE
+// //  https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=QUERY_HERE
 
-module.exports = function(robot) {
-    robot.respond(/kitty me/i, function(msg){
-        var today = new Date();
+// module.exports = function(robot) {
+//     robot.respond(/kitty me/i, function(msg){
+//         var today = new Date();
 
-        msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-    });
-}
+//         msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+//     });
+// }
 
-var giffy = function(search) {
+// var giffy = function(search) {
 
-// request.get({ type: json, url: 'somesite.com' }, function...)
+// // request.get({ type: json, url: 'somesite.com' }, function...)
 
 
-var json = JSON.parse(data);
-};
+// var json = JSON.parse(data);
+// };

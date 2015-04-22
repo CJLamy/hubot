0d678b31d58a77bcc413ef18b06b9eb51fd2ac69
Cedric Lamy <lamycedric@gmail.com>
2015-04-22 15:39:18 -0700

    added kitty me js

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,21 @@
-// //  https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=QUERY_HERE
+//  https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=QUERY_HERE
 
-// module.exports = function(robot) {
-//     robot.respond(/kitty me/i, function(msg){
-//         var today = new Date();
+module.exports = function(robot) {
+  robot.respond(/kitty me/i, function(msg) {
+    var request = require('request');
+    var json;
+    var website = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty";
+    request.get({ url: website }, function (error, response, body) {
+      if (error) {
+        msg.reply("Attempt to pull link failed");
+      }
+      json = JSON.parse(body);
+      msg.reply(json.image_original_url);
+    });
+  });
+}
 
-//         msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-//     });
-// }
-
-// var giffy = function(search) {
-
-// // request.get({ type: json, url: 'somesite.com' }, function...)
 
 
-// var json = JSON.parse(data);
-// };
+// msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+// request.get({ type: json, url: 'somesite.com' }, function...)

824cd4ee90a22d495cbe42bdd85888702d45a852
Cedric Lamy <lamycedric@gmail.com>
2015-04-22 15:45:49 -0700

    Fixed bug and moved to kitty.js

--- scripts/kitty.js
+++ scripts/kitty.js
@@ -1,5 +1,3 @@
-//  https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=QUERY_HERE
-
 module.exports = function(robot) {
   robot.respond(/kitty me/i, function(msg) {
     var request = require('request');
@@ -10,12 +8,6 @@ module.exports = function(robot) {
         msg.reply("Attempt to pull link failed");
       }
       json = JSON.parse(body);
-      msg.reply(json.image_original_url);
-    });
+      msg.reply(json.data.image_original_url);    });
   });
 }
-
-
-
-// msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-// request.get({ type: json, url: 'somesite.com' }, function...)

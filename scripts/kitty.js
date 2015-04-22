f77578e05a25dc32e7af44da951dff9ecf051754
Cedric Lamy <lamycedric@gmail.com>
2015-04-22 15:56:27 -0700

    fixed for some style

--- scripts/kitty.js
+++ scripts/kitty.js
@@ -1,15 +1,17 @@
-//Makes a request to api.giphy to find a kitty gif
+// Makes a request to api.giphy to find a kitty gif.
 module.exports = function(robot) {
   robot.respond(/kitty me/i, function(msg) {
     var request = require('request');
     var json;
-    var website = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty";
+    var website = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty';
     request.get({ url: website }, function (error, response, body) {
-      //If the request somehow fails just for reference
+      // If the request fails, lets the user know.
       if (error) {
-        msg.reply("Attempt to pull link failed");
+        msg.reply('Attempt to pull link failed');
+      } else {
+        json = JSON.parse(body);
+        msg.reply(json.data.image_original_url);
       }
-      json = JSON.parse(body);
-      msg.reply(json.data.image_original_url);    });
+    });
   });
 }

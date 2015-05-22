f79b4aa0d675cefca323a18848ce4580572e0caa
Cedric Lamy <lamycedric@gmail.com>
2015-05-22 16:32:20 -0700

    clean up kitty.js

--- scripts/kitty.js
+++ scripts/kitty.js
@@ -1,5 +1,5 @@
 //KITTIES!
-module.exports = function(robot) {
+module.exports = function (robot) {
 
   // Makes a request to api.giphy to find a kitty gif.
   var crawl = function (error, response, body) {
@@ -17,14 +17,14 @@ module.exports = function(robot) {
   var website = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty';
 
   // This command will return only one kitty gif
-  robot.respond(/kitty me/i, function(msg) {
+  robot.respond(/kitty me/i, function (msg) {
     request.get({ url: website }, function (error, response, body) {
       msg.send(crawl(error, response, body));
     });
   });
 
   // This command will return five kitty gif
-  robot.respond(/kitty bomb/i, function(msg) {
+  robot.respond(/kitty bomb/i, function (msg) {
     for (i = 0; i < 5; i++) {
       request.get({ url: website }, function (error, response, body) {
         msg.send(crawl(error, response, body));

38f61b8d8ac35be74fd7c6598751d2df92939c9a
Cedric Lamy <lamycedric@gmail.com>
2015-05-22 15:50:20 -0700

    added kitty bomb

--- scripts/kitty.js
+++ scripts/kitty.js
@@ -15,3 +15,23 @@ module.exports = function(robot) {
     });
   });
 };
+
+// Bomb with ugly kitties!
+module.exports = function(robot) {
+  robot.respond(/kitty bomb/i, function(msg) {
+    var request = require('request');
+    var json;
+    var website = 'http://tv.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=ugly-kitty';
+    for (i = 0; i < 5; i++) {
+        request.get({ url: website }, function (error, response, body) {
+        // If the request fails, lets the user know.
+        if (error) {
+          msg.send('Attempt to pull link failed');
+        } else {
+          json = JSON.parse(body);
+          msg.send(json.data.image_original_url);
+        }
+      });
+    }
+  });
+};

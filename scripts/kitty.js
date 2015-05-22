efc94009b91d174e704e91d76ae62e8cc52ee9b8
Cedric Lamy <lamycedric@gmail.com>
2015-05-22 16:10:08 -0700

    clean up kitty.js

--- scripts/kitty.js
+++ scripts/kitty.js
@@ -1,34 +1,25 @@
 // Makes a request to api.giphy to find a kitty gif.
+var crawl = function (error, response, body) {
+  // If the request fails, lets the user know.
+  if (error) {
+    msg.send('Attempt to pull link failed');
+  } else {
+    json = JSON.parse(body);
+    msg.send(json.data.image_original_url);
+  }
+};
+
 module.exports = function(robot) {
+  var request = require('request');
+  var json;
+  var website = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty';
   robot.respond(/kitty me/i, function(msg) {
-    var request = require('request');
-    var json;
-    var website = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty';
-    request.get({ url: website }, function (error, response, body) {
-      // If the request fails, lets the user know.
-      if (error) {
-        msg.send('Attempt to pull link failed');
-      } else {
-        json = JSON.parse(body);
-        msg.send(json.data.image_original_url);
-      }
-    });
+    request.get({ url: website }, crawl (error, response, body));
   });
 
   robot.respond(/kitty bomb/i, function(msg) {
-    var request = require('request');
-    var json;
-    var website = 'http://tv.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty';
     for (i = 0; i < 5; i++) {
-        request.get({ url: website }, function (error, response, body) {
-        // If the request fails, lets the user know.
-        if (error) {
-          msg.send('Attempt to pull link failed');
-        } else {
-          json = JSON.parse(body);
-          msg.send(json.data.image_original_url);
-        }
-      });
+        request.get({ url: website }, crawl (error, response, body));
     }
   });
 };
